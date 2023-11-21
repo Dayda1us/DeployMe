26cde698a284328f392966e063fc31a5b60f6a72
Charles <145280599+Dayda1us@users.noreply.github.com>
2023-11-21 15:53:30 -0800

    Add files via upload

--- Register-MicrosoftKey.ps1
+++ Register-MicrosoftKey.ps1
@@ -3,49 +3,56 @@ function Register-MicrosoftKey {
     Param()
     BEGIN {
         $Title       = 'KeyDeploy'
-        $Description = 'You are about to launch KeyDeploy. If this PC is not connected to the deployment station, please power off the PC and move it to the deployment station.'
+        $Description = 'You are about to launch KeyDeploy. If this PC is not connected to the deployment station, please move the PC to the deployment station.'
         $Yes         = New-Object System.Management.Automation.Host.ChoiceDescription "&Y", "Launch KeyDeploy"
-        $No          = New-Object System.Management.Automation.Host.ChoiceDescription "&N", "Don't launch KeyDeploy. You will be prompt if you would like to shutdown your PC."
-        $Options     = [System.Management.Automation.Host.ChoiceDescription[]]($Yes,$No)
+        $No          = New-Object System.Management.Automation.Host.ChoiceDescription "&N", "Skip this step. This option will open a window the KeyDeploy directory if available."
+        $Shutdown    = New-Object System.Management.Automation.Host.ChoiceDescription "&Shutdown", "Shutdown your PC. Use this option if you're deploying a desktop."
+        $Options     = [System.Management.Automation.Host.ChoiceDescription[]]($Yes, $No, $Shutdown)
         $Default     = 1    # 0 = Yes, 1 = No
 
         do {
             $KDResponse = $Host.UI.PromptForChoice($Title, $Description, $Options, $Default)
-            if ($KDResponse -eq 0) {break}
+            if ($KDResponse -eq 0) {
+                return 0 | Out-Null
+            }
+            elseif ($KDResponse -eq 2) {
+                return 2 | Out-Null
+            }
         } until ($KDResponse -eq 1)
     } #BEGIN
     PROCESS {
-        if ($KDResponse -eq 0) {
-            Write-Output "Opening KeyDeploy..."
-            <# TEST ONLY #> Invoke-Item -Path "$env:WINDIR\notepad.exe"
-            #Invoke-Item -Path "$env:WINDIR\MARFO_SCRIPTS\Startup\DTStartup.exe"
-            $Process = 'DTStartup'
-            do {
-                Write-Output "$Process is open"
-                start-sleep -Seconds 2
-            } while ((Get-Process -name $Process -EA SilentlyContinue).name -contains $Process) #dowhile
-        }
-        else {
-            $Description = "Would you like to turn off your PC?"
-            $Yes         = New-Object System.Management.Automation.Host.ChoiceDescription "&Y", "Shut down the PC."
-            $No          = New-Object System.Management.Automation.Host.ChoiceDescription "&N", "Do not shut down the PC."
-            $Options     = [System.Management.Automation.Host.ChoiceDescription[]]($Yes,$No)
-            $Default     = 1    # 0 = Yes, 1 = No
-            do {
-                $ShutdownResponse = $Host.UI.PromptForChoice($Title, $Description, $Options, $Default)
-                if ($ShutdownResponse -eq 0) {
-                    Write-Warning 'PC is shutting down'
-                    start-sleep -Seconds 2
-                    #Stop-Computer -Force
-                    break
+        switch ($KDResponse) {
+            {$KDResponse -eq 0} {
+                Write-Output "Opening KeyDeploy..."
+                <# TEST ONLY #> Invoke-Item -Path "$env:WINDIR\notepad.exe"
+                if ((Test-Path -Path "$env:WINDIR\MARFO_SCRIPTS\Startup\DTStartup.exe") -eq $true) {
+                    #Invoke-Item -Path "$env:WINDIR\MARFO_SCRIPTS\Startup\DTStartup.exe"
                 }
-            } until ($ShutdownResponse -eq 1)
-        } #if ($KDResponse -eq 0)
+                $Process = 'notepad'
+                do {
+                    start-sleep -Seconds 1
+                } while ((Get-Process -name $Process -EA SilentlyContinue).name -contains $Process) #dowhile
+            } #{$KDResponse -eq 0}
+            {$KDResponse -eq 2} {
+                Write-Warning 'PC is shutting down'
+                start-sleep -Seconds 2
+                Stop-Computer -Force -WhatIf
+                return 2 | Out-Null
+            } #{$KDResponse -eq 2}
+            default {
+                return 1 | Out-Null
+            } #default
+        } #switch ($KDResponse)
     }# PROCESS
     END {
-        if ($KDResponse -ne 0 -and $ShutdownResponse -ne 0) {
+        if ($KDResponse -eq 1) {
             Write-Output 'OK! Skipping product key deployment'
-        } elseif ($KDResponse -ne 0 -and $ShutdownResponse -ne 1) {
+            # This if statement will work if the KeyDeploy files are available.
+            if ((Test-Path -Path "$env:WINDIR\MARFO_SCRIPTS\Startup\") -eq $true) {
+                Invoke-Item -Path "$env:WINDIR\MARFO_SCRIPTS\Startup\"
+            } #if ((Test-Path -Path "$env:WINDIR\MARFO_SCRIPTS\Startup\") -eq $true)
+        }
+        elseif ($KDResponse -eq 2) {
             Write-Output 'PC currently shutting down.'
             break
         } else {
@@ -53,7 +60,7 @@ function Register-MicrosoftKey {
             Write-Output "`nMake sure to leave a put a square holographic Microsoft Authorized Refurbisher sticker on the bottom of the unit, or as close to the original Windows sticker as possible."
             Write-Output "If this is a citzenship PC, only put a Microsoft Office for Citizenship key sticker. Citizenship PCs does not get a holographic sticker.`n"
             Write-Warning "If you are deploying a non-citzenship PC, please make sure to report this to the MDOS Smart Client."
-            start-sleep 10
-        }#if/elseif/else ($KeyDeployPrompt -ne 0 -and $ShutdownResponse -ne 0)
+            Start-Sleep -Seconds 10
+        }#if/elseif/else ($KDResponse -eq 1)
     }# END
-}
+} #function Register-MicrosoftKey
